4b67afe94d2811a5d954bb33c8e1985139c05985
linksapprentice1 <clamsmasha1992@gmail.com>
2017-10-01 11:50:05 -0400

    dump.sql
    Database that has Duration table with zeroed-out values for columns

---
+++ dump.sql
@@ -0,0 +1,105 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: root
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `root`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `root` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `root`;
+
+--
+-- Table structure for table `Duration`
+--
+
+DROP TABLE IF EXISTS `Duration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Duration` (
+  `one_month` int(6) DEFAULT NULL,
+  `three_months` int(6) DEFAULT NULL,
+  `three_to_six_months` int(6) DEFAULT NULL,
+  `one_year` int(6) DEFAULT NULL,
+  `five_years` int(6) DEFAULT NULL,
+  `ten_years` int(6) DEFAULT NULL,
+  `twenty_years` int(6) DEFAULT NULL,
+  `fifty_years` int(6) DEFAULT NULL,
+  `seventy_years` int(6) DEFAULT NULL,
+  `seventy_plus_years` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Duration`
+--
+
+LOCK TABLES `Duration` WRITE;
+/*!40000 ALTER TABLE `Duration` DISABLE KEYS */;
+INSERT INTO `Duration` VALUES (0,0,0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `Duration` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `root`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `root` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `root`;
+
+--
+-- Table structure for table `Duration`
+--
+
+DROP TABLE IF EXISTS `Duration`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Duration` (
+  `one_month` int(6) DEFAULT NULL,
+  `three_months` int(6) DEFAULT NULL,
+  `three_to_six_months` int(6) DEFAULT NULL,
+  `one_year` int(6) DEFAULT NULL,
+  `five_years` int(6) DEFAULT NULL,
+  `ten_years` int(6) DEFAULT NULL,
+  `twenty_years` int(6) DEFAULT NULL,
+  `fifty_years` int(6) DEFAULT NULL,
+  `seventy_years` int(6) DEFAULT NULL,
+  `seventy_plus_years` int(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Duration`
+--
+
+LOCK TABLES `Duration` WRITE;
+/*!40000 ALTER TABLE `Duration` DISABLE KEYS */;
+INSERT INTO `Duration` VALUES (0,0,0,0,0,0,0,0,0,0);
+/*!40000 ALTER TABLE `Duration` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-01 11:29:50
