3366f38d676971d59faa94149f73020e1a83a239
linksapprentice1 <clamsmasha1992@gmail.com>
2017-10-01 11:52:10 -0400

    Create index.php
    Option selection

---
+++ html/index.php
@@ -0,0 +1,20 @@
+
+<div style="margin: 0 auto; width:400px">
+  <h2>When will Jeffrey get a job?</h2>
+</div>
+<div style="margin: 0 auto; width:250px">
+<form action="../process.php">
+  <input type="radio" name="duration" value="one_month"> Less than 1 month<br>
+  <input type="radio" name="duration" value="three_months"> Less than 3 months<br>
+  <input type="radio" name="duration" value="three_to_six_months"> Less than 6 months<br>
+  <input type="radio" name="duration" value="one_year"> Less than a year<br>
+  <input type="radio" name="duration" value="five_years"> Less than 5 years<br>
+  <input type="radio" name="duration" value="ten_years"> Less than 10 years<br>
+  <input type="radio" name="duration" value="twenty_years"> Less than 20 years<br>
+  <input type="radio" name="duration" value="fifty_years"> Less than 50 years<br>
+  <input type="radio" name="duration" value="seventy_years"> Less than 70 years<br>
+  <input type="radio" name="duration" value="seventy_plus_years"> Greater than 70 years<br>
+  <br>
+  <input style="text-align:center" type="submit" value="Submit">
+</form>
+</div>
